b72c4fea6be3b7a2fe101592f50d5b4e8d30dd5d
ravindra <rpatel.gen@gmail.com>
2021-08-22 23:54:15 +0530

    first commit

---
+++ src/test/java/qaclickacadmy/Mavenjava/RESTAPITest.java
@@ -0,0 +1,21 @@
+package qaclickacadmy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+	
+	
+}
